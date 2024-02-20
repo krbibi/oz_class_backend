48fff116cc2014c0d2982f793e6109c613267ccd
bibi <153698028+krbibi@users.noreply.github.com>
2024-02-20 14:38:27 +0900

    Delete Django/oz_django_backend directory

--- Django/oz_django_backend/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-from rest_framework.authtoken.views import obtain_auth_token
-urlpatterns = [
-    path("", views.Users.as_view()), # api/v1/users
-    path("myinfo", views.MyInfo.as_view()), # api/v1/users
-    path("getToken",obtain_auth_token)
-]
